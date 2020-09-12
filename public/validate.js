a2dd0f4f8add179d976d1c12428cd23228994723
Hanna Han <48030964+hsh803@users.noreply.github.com>
2020-09-12 12:52:54 +0200

    Add files via upload

--- public/validate.js
+++ public/validate.js
@@ -1,19 +1,19 @@
 var send = document.getElementById("submit");
 send.addEventListener("click", formValidation);
 
-function formValidation(){
+function formValidation(e){
   //Contact value
   var firstname = document.getElementById("firstname").value;
   var lastname = document.getElementById("lastname").value;
   var email = document.getElementById("email").value;
-  var organisation = document.getElementById("organization").value;
+  var organization = document.getElementById("organization").value;
   
   //Presentation value
   var lecture = document.getElementById("lecture").checked;
   var seminar = document.getElementById("seminar").checked;
   var discussion = document.getElementById("discussion").checked;
   var title = document.getElementById("title").value;
-  var maxword = document.getElementById("message").value.length;
+  var msg = document.getElementById("message").value.length;
 
   //Email value
   var pattern =  /^([a-zA-Z0-9_.-])+@([a-zA-Z0-9_.-])+\.([a-zA-Z])+([a-zA-Z])+/;
@@ -21,29 +21,29 @@ function formValidation(){
 
   //Contact validation
   if (firstname === "" || lastname === "" || email === "" || organization === "") {
-    event.preventDefault();
+    e.preventDefault();
     alert ("All contact inforamtion must be filled.");
     return false;
   }
 
   //Email validation
   if (isValid === false) {
-    event.preventDefault();
+    e.preventDefault();
     alert ("E-mail has wrong format.");
     return false;
   }
 
   //Title validation
   if ((lecture === true && title === "") || (seminar === true && title === "") || (discussion === true && title === "")) {
-    event.preventDefault();
+    e.preventDefault();
     alert ("Title of presentation must be filled.");
     return false;
   }
   
   //Message validation
-  if (maxword > 200) {
-    event.preventDefault();
-    alert ("You can write max 200 words.");
+  if (msg > 200) {
+    e.preventDefault();
+    alert ("You can write max 200 signs.");
     return false;
   }
 }
